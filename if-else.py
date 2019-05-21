df53f65b00547cdca0a05cf076f2abeecec0e7b0
agrawalanu <50593123+agrawalanu@users.noreply.github.com>
2019-05-22 03:24:20 +0530

    if-else.py

---
+++ if-else.py
@@ -0,0 +1,88 @@
+d = 10>9
+print(d)
+
+x=100
+if x==100:
+    print("x is is equal to", x)
+
+    if x == 100:
+        print("x is is equal to")
+        print(x)
+print('finish')
+if x>0:
+    print("x is positive")
+else:
+    print("x is negative")
+
+
+x =-100
+if x == 100:
+    print("x is is equal to", x)
+
+if x == 100:
+    print("x is is equal to")
+    print(x)
+print('finish')
+
+if x > 0:
+     print("x is positive")
+else:
+     print("x is negative")
+     
+     
+     name = input("enter a name:")
+
+if name == "max":
+    print("name entered is:",name)
+elif name == "leo":
+    print("name entered is:",name)
+elif name == "roy":
+    print("name entered is:",name)
+elif name == "allie":
+    print("name entered is:",name)
+else:
+    print("the name enterd is invalid"
+    
+    
+    
+if name == "max":
+    print("name entered is:",name)
+if name == "max":                        #now this is a new loop
+    print("name entered is:",name)
+elif name == "roy":
+    print("name entered is:",name)
+elif name == "allie":
+    print("name entered is:",name)
+else:
+    print("the name enterd is invalid")
+    
+    
+    
+x = 10
+if x<0:
+    print('x is negative')
+else:
+    print('x is positive')
+    if (x%2) ==0:                      #nesting
+        print('x is even')
+    else:
+        print("x is odd")
+        
+        
+        
+        
+if x>0:
+    print('x is positive')
+    if (x % 2) == 0:  # nesting
+        print('x is even')
+    else:
+        print("x is odd")
+
+else:
+    print('x is negative')
+    
+    
+    
+    
+    
+    
